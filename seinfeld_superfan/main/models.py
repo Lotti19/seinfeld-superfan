55d1e7c1ab6ae0783759fb572daef82254dc5def
Jonathan Bellotti <bellottij@gmail.com>
2021-05-21 21:22:09 -0500

    added post and comment model

--- seinfeld_superfan/main/models.py
+++ seinfeld_superfan/main/models.py
@@ -45,3 +45,15 @@ class User(models.Model):
     password = models.CharField(max_length = 255)
 
     objects = UserManager()
+    
+class Post(models.Model):
+    
+    message = models.CharField(max_length=255)
+    poster = models.ForeignKey(User, related_name='user_messages', on_delete=models.CASCADE)
+    user_likes = models.ManyToManyField(User, related_name='liked_posts')
+    
+class Comment(models.Model):
+    
+    comment = models.CharField(max_length=255)
+    poster = models.ForeignKey(User, related_name='user_comments', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='post_comments', on_delete=models.CASCADE)
