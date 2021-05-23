4f43e1e0da5b02bc7e84dabcbdf359c56c89003b
Jonathan Bellotti <bellottij@gmail.com>
2021-05-23 16:54:48 -0500

    added post, post_success, post_comment, and add_like methods to views file and updated corresponing urls

--- seinfeld_superfan/main/urls.py
+++ seinfeld_superfan/main/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('community', views.community),
+    path('post', views.post),
+    path('post_success', views.post_success),
+    path('post_comment', views.post_comment),
+    path('add_like', views.add_like),
 ]
--- seinfeld_superfan/main/views.py
+++ seinfeld_superfan/main/views.py
@@ -1,8 +1,48 @@
 from django.shortcuts import render, redirect
+
 from . models import *
+
 from django.contrib import messages
+
 import bcrypt
 
 def index(request):
     return render(request, 'index.html')
+
+def community(request):
+    
+    context = {
+        'posts': Post.objects.all()
+    }
+    
+    return render(request, 'community.html', context)
+
+def post(request):
+    
+    Post.objects.create(message=request.POST['post'], poster=User.objects.get(id=request.session['user_id']))
+    
+    return redirect('/post_success')
+
+def post_success(request):
+    
+    context = {
+        'posts': Post.objects.all()
+    }
+    
+    return render(request, 'community.html', context)
+
+def post_comment(request, id):
+    
+    poster = User.objects.get(id=request.session['user_id'])
+    post = Post.objects.get(id=id)
+    Comment.objects.create(comment=request.POST['comment'], poster=poster, post=post)
+    return redirect('/post_success')
+
+def add_like(request, id):
+    
+    liked_post = Post.objects.get(id=id)
+    user_liking = User.objects.get(id=request.session['user_id'])
+    liked_post.user_likes.add(user_liking)
+    
+    return redirect('/post_success')
 
