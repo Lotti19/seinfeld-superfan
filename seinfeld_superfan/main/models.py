1d59efdc64f10bd145565f6ea0eb0862d80955a8
Jonathan Bellotti <bellottij@gmail.com>
2021-05-23 17:08:12 -0500

    added register and login html files, slight update to user model

--- seinfeld_superfan/main/models.py
+++ seinfeld_superfan/main/models.py
@@ -58,7 +58,10 @@ class User(models.Model):
     first_name = models.CharField(max_length = 255)
     last_name = models.CharField(max_length = 255)
     email = models.CharField(max_length = 255)
-    password = models.CharField(max_length = 255)
+    password = models.CharField(max_length = 100)
+    confirm_password = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     objects = UserManager()
     
