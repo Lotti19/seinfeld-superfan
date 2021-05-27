fbde2e261633a0d39fd74d588a60ede4065fcbd1
Jonathan Bellotti <bellottij@gmail.com>
2021-05-27 17:52:09 -0500

    store images all rendering, cart images rendering, as well as cart and item totals

--- seinfeld_superfan/store/views.py
+++ seinfeld_superfan/store/views.py
@@ -23,10 +23,12 @@ def cart(request):
         items = order.orderitem_set.all()
     else:
         items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
         
 
     context = {
-        'items':items
+        'items': items,
+        'order': order
         }
     
     return render(request, 'cart.html', context)
